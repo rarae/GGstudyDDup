7ac7add8ba5ffd4db16ed5ae3295fbdcd95f9436
hyp <374682827@qq.com>
2021-07-03 19:58:57 +0800

    11111

--- 1.js
+++ 1.js
@@ -1,47 +1,48 @@
-const createClock = (log, time) => new Promise(resolve => {
-    setTimeout(() => {
-        resolve(log);
-    }, time);
-});
+const cb = (n) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(n);
+            console.log(n);
+        }, Math.random() * 3000)
+    });
+}
+let j = 0;
+const arr = new Array(20).fill('').map(() => cb.bind(null, j++));
 
-let p1 = createClock(1, 5000);
-let p2 = createClock(2, 1000);
-let p3 = createClock(3, 1000);
+function dispatch(arr, max) {
+    let seq = 0,
+        received = 0,
+        total = arr.length,
+        res = new Array(total).fill(0);
 
-// 1. 串行调用就是要达到这个效果  依次输出 1 2 3
-// p1.then(r=>{
-//     console.log(r);
-//     return p2;
-// }).then(r=>{
-//     console.log(r);
-//     return p3;
-// }).then(r=>{
-//     console.log(r);
-// })
+    return new Promise(resolve => {
+        const next = () => {
+            if (arr.length <= 0) return;
 
-// 2. 使用forEach实现
-Promise.all = function (arr) {
-    let p = Promise.resolve('OK'); // 先生成一个实例
-    arr.forEach(item => {
-        // 第一个then用来接收'OK'，其实是辅助的promise实例，主要目的是在其resolve里面返回一个新的promise实例
-        // 第二个then才是真正的要执行的promise实例
-        p = p.then(() => item).then(result => {
-            console.log(result);
-            return 'OK' // 其实不返回也可以，因为执行成功浏览器会默认返回成功的promise实例
-        })
+            let cur = seq;
+            seq++;
+            let task = arr.shift();
+            task().then(result => {
+                res[cur] = result;
+            }, reason => {
+                res[cur] = reason;
+            }).finally(() => {
+                received++;
+                if (arr.length > 0) {
+                    next();
+                } else if (received >= total) {
+                    resolve(res);
+                }
+
+            })
+        }
+
+        while (seq < max) {
+            next();
+        }
     })
-    return p;
 }
 
-Promise.all([p1, p2, p3]).then(result => {
-    console.log(result);  // 输出的就是 'ok'
-});
-
-// 3. 使用async/await实现
-async function seq (arr) {
-    for (const item of arr) {
-        let res = await item;
-        console.log(res); // 依次输出1 2 3
-    }
-}
-seq([p1, p2, p3]);
+dispatch(arr, 5).then(result => {
+    console.log(result);
+})
--- js.md
+++ js.md
@@ -814,6 +814,57 @@ function AsyncFunc(generator, ...params) {
 AsyncFunc(generator, 0, 5);
 ```
 
+## promise并发串行 递归
+```javascript
+const cb = (n) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(n);
+            console.log(n);
+        }, Math.random() * 3000)
+    });
+}
+let j = 0;
+const arr = new Array(20).fill('').map(() => cb.bind(null, j++));
+
+function dispatch(arr, max) {
+    let seq = 0,
+        received = 0,
+        total = arr.length,
+        res = new Array(total).fill(0);
+
+    return new Promise(resolve => {
+        const next = () => {
+            if (arr.length <= 0) return;
+
+            let cur = seq;
+            seq++;
+            let task = arr.shift();
+            task().then(result => {
+                res[cur] = result;
+            }, reason => {
+                res[cur] = reason;
+            }).finally(() => {
+                received++;
+                if (arr.length > 0) {
+                    next();
+                } else if (received >= total) {
+                    resolve(res);
+                }
+            })
+        }
+
+        while (seq < max) {
+            next();
+        }
+    })
+}
+
+dispatch(arr, 5).then(result => {
+    console.log(result);
+})
+```
+
 ## 手写promise
 ```javascript cmd='node'
 // es5写法 还没有实现顺延机制，对付面试足够了, 顺延机制看视频45
