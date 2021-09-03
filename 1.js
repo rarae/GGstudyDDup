462c169f7cdff5ce8afd07f6087346549f3f42a9
hyp <374682827@qq.com>
2021-09-03 23:11:58 +0800

    1

--- 1.js
+++ 1.js
@@ -1,20 +1,19 @@
-var longestPalindromeSubseq = function (s) {
-  const l = s.length,
-    dp = new Array(l);
-  for (let i = 0; i < l; i++) {
-    dp[i] = new Array(l).fill(0);
-    dp[i][i] = 1;
-  }
+var per = function (s) {
+  const res = [],
+    l = s.length;
 
-  for (let i = l - 1; i >= 0; i--) {
-    for (let j = i + 1; j < l; j++) {
-      if (s[i] === s[j]) {
-        dp[i][j] = dp[i + 1][j - 1] + 2;
-      } else {
-        dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
-      }
+  function dfs(ed, startIndex) {
+    if (ed.length === 3 && ed.reduce((pre, cur, index, arr) => pre + cur)===7) {
+      res.push([...ed]);
+    }
+    for (let i = startIndex; i < l; i++) {
+      ed.push(s[i]);
+      dfs(ed, i + 1);
+      ed.pop();
     }
   }
-  return dp[0][l - 1];
+  dfs([], 0);
+  console.log(res);
 };
-console.log(longestPalindromeSubseq("bbbab"));
+
+per([1, 2, 3, 4, 5, 6, 7, 8, 9]);
