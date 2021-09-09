eef699c3adf676ab20dddf54beb6be2dac836ffb
hyp <374682827@qq.com>
2021-09-09 15:05:27 +0800

    1.md

--- 1.js
+++ 1.js
@@ -1,19 +1,2 @@
-var per = function (s) {
-  const res = [],
-    l = s.length;
-
-  function dfs(ed, startIndex) {
-    if (ed.length === 3 && ed.reduce((pre, cur, index, arr) => pre + cur)===7) {
-      res.push([...ed]);
-    }
-    for (let i = startIndex; i < l; i++) {
-      ed.push(s[i]);
-      dfs(ed, i + 1);
-      ed.pop();
-    }
-  }
-  dfs([], 0);
-  console.log(res);
-};
-
-per([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(0.25 + 0.5 === 0.75);
+console.log(0.1 + 0.2 === 0.3);
--- 1.py
+++ 1.py
@@ -1,13 +1,7 @@
-import os
-import io
-import sys
-sys.stdout=io.TextIOWrapper(sys.stdout.buffer,encoding='utf8')
-os.chdir('e:\\web\\GGstudyDDup\\')
+M_P, M_N, M_TP = 1076, 1381, 625
+S_P, S_N, S_TP = 1089, 1699, 233
 
-f1 = open('1.txt', 'r', encoding='utf-8').readlines()
-f2 = open('2.txt', 'r', encoding='utf-8').readlines()
-
-for more in f2:
-  if more not in f1:
-    print(more)
-
+p = (M_TP+S_TP)/(M_P+S_P)
+r = (M_TP+S_TP)/(M_N+S_N)
+f1 = (2.0 * p * r) / (p + r)
+print(p, r, f1)
