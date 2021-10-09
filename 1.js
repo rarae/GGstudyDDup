efc3881c27ef78b6568d4137e4eb65c58bec0d02
hyp <374682827@qq.com>
2021-10-09 10:33:01 +0800

    1

--- 1.js
+++ 1.js
@@ -1,12 +1,27 @@
-var positions1 = [1, 3, 5];
-var positions2 = [1, 5];
-var first = positions1[0],
-  last = positions1[positions1.length - 1];
-res = Infinity;
-for (var i = 0; i < positions2.length; i++) {
-  var e = positions2[i];
-  var left = e - first;
-  var right = last - e;
-  res = Math.min(res, Math.max(left, right));
+function f(nums) {
+  const target = 1;
+  const index = [];
+  // 求出所有target所在的位置
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target) {
+      index.push(i + 1);
+    }
+  }
+  // 其实就是找出在固定窗口内，非target元素的最少数量
+  const count = index.length;
+  let res = Infinity;
+  for (let i = 0; i < index.length; i++) {
+    const left = index[i],
+      right = left + count - 1;
+    let tmpRes = 0;
+    for (let j = left; j <= right; j++) {
+      if (index.indexOf(j) === -1) {
+        tmpRes++;
+      }
+    }
+    res = Math.min(res, tmpRes);
+  }
+  return res;
 }
-console.log(res);
+
+console.log(f([1, 1, 0, 0, 1]));
